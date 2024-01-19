a58bf10de619704efe92e56f9414ad05f326f49e
sbhushan1 <139586802+sbhushan1@users.noreply.github.com>
2024-01-19 13:56:56 +0000

    Stack and Heap memory

--- 01_basics/dataTypes-Summary.js
+++ 01_basics/dataTypes-Summary.js
@@ -23,3 +23,30 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "shekharbhushandotcom"
+
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "shekhar@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
