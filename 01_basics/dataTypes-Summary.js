5963cfa44acf46d77b91a6417812a2daad43619b
sbhushan1 <139586802+sbhushan1@users.noreply.github.com>
2024-01-19 13:39:43 +0000

    summary

--- 01_basics/dataTypes-Summary.js
+++ 01_basics/dataTypes-Summary.js
@@ -2,6 +2,24 @@
 
 // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj", "Doga"];
+
+let myObj = {
+    name: "Shekhar",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
