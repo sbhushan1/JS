f41a72a8c202955da38ad8f0be04056417b7325c
sbhushan1 <139586802+sbhushan1@users.noreply.github.com>
2024-01-21 07:10:18 +0000

    mathsAndNUm

--- 01_basics/02_nums_and_math.js
+++ 01_basics/02_nums_and_math.js
@@ -1,4 +1,4 @@
- const score = 400
+  const score = 400
  //console.log(score);
 
  const balance = new Number(100)
@@ -15,7 +15,19 @@
  //console.log(hundreds.toLocaleString('en-IN'));
 
  // ++++++++++++++++ Maths ++++++++++++++++
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor((Math.random()*10) + 1));
+
+// To get number between min and max
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
